3822cdcc94cd76b328001e7be6e6d036039499df
comp500 <comp500@users.noreply.github.com>
2020-06-26 22:05:53 +0100

    Fix support for newer Java by using VarHandle to bypass the reflection blacklist

--- src/main/java/link/infra/jumploader/specialcases/FabricLoaderReflectionHack.java
+++ src/main/java/link/infra/jumploader/specialcases/FabricLoaderReflectionHack.java
@@ -5,6 +5,8 @@ import link.infra.jumploader.util.RegexUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.lang.reflect.Field;
 import java.net.URL;
 import java.net.URLStreamHandler;
@@ -15,25 +17,67 @@ import java.util.regex.Pattern;
 
 public class FabricLoaderReflectionHack implements ReflectionHack {
 	private final Logger LOGGER = LogManager.getLogger();
+	private static final double JAVA_VERSION = Double.parseDouble(System.getProperty("java.specification.version", "0"));
+
+	private interface ReflectionVersionHandler {
+		void setClassLoader(ClassLoader loader) throws NoSuchFieldException, IllegalAccessException;
+		void resetInstalledProviders() throws NoSuchFieldException, IllegalAccessException;
+	}
+
+	private static class ReflectionVersionHandler8 implements ReflectionVersionHandler {
+		@Override
+		public void setClassLoader(ClassLoader loader) throws NoSuchFieldException, IllegalAccessException {
+			Field scl = ClassLoader.class.getDeclaredField("scl");
+			scl.setAccessible(true);
+			scl.set(null, loader);
+		}
+
+		@Override
+		public void resetInstalledProviders() throws NoSuchFieldException, IllegalAccessException {
+			Field installedProviders = FileSystemProvider.class.getDeclaredField("installedProviders");
+			installedProviders.setAccessible(true);
+			installedProviders.set(null, null);
+			Field loadingProviders = FileSystemProvider.class.getDeclaredField("loadingProviders");
+			loadingProviders.setAccessible(true);
+			loadingProviders.set(null, false);
+		}
+	}
+
+	@SuppressWarnings("Since15")
+	private static class ReflectionVersionHandler9 implements ReflectionVersionHandler {
+		@Override
+		public void setClassLoader(ClassLoader loader) throws NoSuchFieldException, IllegalAccessException {
+			VarHandle handle = MethodHandles.privateLookupIn(ClassLoader.class, MethodHandles.lookup()).findStaticVarHandle(ClassLoader.class, "scl", ClassLoader.class);
+			handle.set(loader);
+		}
+
+		@Override
+		public void resetInstalledProviders() throws NoSuchFieldException, IllegalAccessException {
+			MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(FileSystemProvider.class, MethodHandles.lookup());
+			VarHandle installedProviders = lookup.findStaticVarHandle(FileSystemProvider.class, "installedProviders", List.class);
+			installedProviders.set((Object) null);
+			VarHandle loadingProviders = lookup.findStaticVarHandle(FileSystemProvider.class, "loadingProviders", boolean.class);
+			loadingProviders.set(false);
+		}
+	}
 
 	private static void reloadFSHandlers(ClassLoader classLoader) throws NoSuchFieldException, IllegalAccessException {
 		// Attempt to load the jimfs protocol handler (required for jar-in-jar) by hacking around the system classloader
-		Field scl = ClassLoader.class.getDeclaredField("scl");
-		scl.setAccessible(true);
-		ClassLoader existingLoader = (ClassLoader) scl.get(null);
-		scl.set(null, classLoader);
+		ClassLoader existingLoader = ClassLoader.getSystemClassLoader();
+		ReflectionVersionHandler handler;
+		if (JAVA_VERSION > 8) {
+			handler = new ReflectionVersionHandler9();
+		} else {
+			handler = new ReflectionVersionHandler8();
+		}
+		handler.setClassLoader(classLoader);
 
 		// Force FileSystemProvider to re-enumerate installed providers
-		Field installedProviders = FileSystemProvider.class.getDeclaredField("installedProviders");
-		installedProviders.setAccessible(true);
-		installedProviders.set(null, null);
-		Field loadingProviders = FileSystemProvider.class.getDeclaredField("loadingProviders");
-		loadingProviders.setAccessible(true);
-		loadingProviders.set(null, false);
+		handler.resetInstalledProviders();
 		FileSystemProvider.installedProviders();
 
 		// Set the system classloader back to the actual system classloader
-		scl.set(null, existingLoader);
+		handler.setClassLoader(existingLoader);
 	}
 
 	@SuppressWarnings("unchecked")
