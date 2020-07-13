128f0515f6ed39a215b7265e6569857bcc5a2d31
comp500 <comp500@users.noreply.github.com>
2020-07-13 23:28:55 +0100

    Replace the version argument when using Twitch with Forge

--- src/main/java/link/infra/jumploader/launch/arguments/ParsedArguments.java
+++ src/main/java/link/infra/jumploader/launch/arguments/ParsedArguments.java
@@ -3,9 +3,11 @@ package link.infra.jumploader.launch.arguments;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class ParsedArguments {
 	private final Map<String, String> argumentsParsed = new HashMap<>();
@@ -56,6 +58,7 @@ public class ParsedArguments {
 		}
 
 		removeFMLArgs(argumentsList);
+		replaceTwitchBrand(getOrDefault("version", ""), argumentsList, gameDir);
 	}
 
 	private static void removeFMLArgs(List<String> argsList) {
@@ -71,6 +74,37 @@ public class ParsedArguments {
 				if (!peek.startsWith("--")) {
 					iter.remove();
 					peek = null;
+				}
+			}
+		}
+	}
+
+	/**
+	 * In a Twitch environment, the game version is usually forge-... which looks confusing to users, as
+	 * it shows up as forge-.../fabric/Fabric in the F3 menu. Here, we replace it with Jumploader.
+	 */
+	private static void replaceTwitchBrand(String version, List<String> argsList, Path gameDir) {
+		// Detect if the version name is a Forge version
+		if (!Pattern.matches("forge-[.\\d]+", version)) {
+			return;
+		}
+		// Detect if we are in a Twitch environment
+		Path installDir = gameDir.resolve(Paths.get("..", "..", "Install"));
+		if (Files.exists(installDir)) {
+			Path gameVersionsDir = installDir.resolve("versions");
+			Path librariesDir = installDir.resolve("libraries");
+			if (!Files.exists(gameVersionsDir) || !Files.exists(librariesDir)) {
+				return;
+			}
+		} else {
+			return;
+		}
+		// Replace the version with Jumploader
+		for (int i = 0; i < argsList.size(); i++) {
+			if (argsList.get(i).equals("--version")) {
+				i++;
+				if (i < argsList.size()) {
+					argsList.set(i, "Jumploader");
 				}
 			}
 		}
